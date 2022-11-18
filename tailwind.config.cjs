688ce72cdcd6f1f7300d1367a2aba7b822a2a70e
Sara Baqla <missatrox44@gmail.com>
2022-11-18 12:57:53 -0600

    added tailwind config, updated footer, links not functional

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: 'black',
+        light: 'white',
+        medium: 'blue'
+      }
+    },
+  },
+  plugins: [],
+}
+
