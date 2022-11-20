6699c1f0aef3f2309e2dec927da661543e3a0ae2
A <andcooke@gmail.com>
2022-11-20 17:05:00 -0600

    simple carousel styling

--- src/components/ReviewCarousel/index.jsx
+++ src/components/ReviewCarousel/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReviewCard from '../ReviewCard';
-
+import "./styles.css";
 
 export default function ReviewCarousel() {
 
   return (
-    <div>
-      <ReviewCard/>
-      <ReviewCard/>
-      <ReviewCard/>
+    <div className="carousel-container">
+      <button className="left-arrow">
+        &lt;
+      </button>
+      <div className="carousel-content-container">
+        <ReviewCard/>
+      </div>
+      <button className="right-arrow">
+        &gt;
+      </button>
+
     </div>
 
   )
