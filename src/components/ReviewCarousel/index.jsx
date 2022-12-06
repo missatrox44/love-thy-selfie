1285b7f3585e583aa263ac920fe478f873af94ef
A <andcooke@gmail.com>
2022-12-06 14:22:48 -0600

    reviews are responsive, next/prev buttons appear and disappear when required

--- src/components/ReviewCarousel/index.jsx
+++ src/components/ReviewCarousel/index.jsx
@@ -7,7 +7,7 @@ const ReviewCarousel = (props) => {
   
   const {children} = props;
 
-  const matches = useMediaQuery('(min-width:640px)');
+  const bigScreen = useMediaQuery('(min-width:640px)');
 
   const [currentIndex, setCurrentIndex] = useState(0)
   const [length, setLength] = useState(children.length)
@@ -21,10 +21,7 @@ const ReviewCarousel = (props) => {
   const next = () => {
     if (currentIndex < (length - 1)) {
         setCurrentIndex(prevState => prevState + 1)
-    }
-    if (children[currentIndex]) {
-      console.log(children[currentIndex])
-    }
+    } 
   }
 
   const prev = () => {
@@ -35,34 +32,38 @@ const ReviewCarousel = (props) => {
 
     return (
         <div className="carousel-container">
-          <span>{`(min-width:600px) matches: ${matches}`}</span>;
+          {/* media query VVVVV*/}
+          {/* <span>{`big screen (min-width:640px): ${bigScreen}`}</span> */}
           <div className="carousel-wrapper">
             {
               currentIndex > 0 &&
               <button onClick={prev} className="left-arrow">
-                  &lt;
+                &lt;
               </button>
             }
             <div className="carousel-content-wrapper">
               <div className="carousel-content"
         
               //the transform below controls how much the carousel moves
-              style={{ transform: `translateX(-${currentIndex * 100}%) translateX(-10px)`}}
-
-              // style={{ transform: `translateX(-${currentIndex * 305}%)`}}
+              style={{ transform: `translateX(-${currentIndex * 100}%)`}}
               >
                 {children}
               </div>
             </div>
           {
-            (currentIndex + 1) ? 
-            // currentIndex < ((length/3)-1) &&
-            currentIndex < length-1 &&
-            <button onClick={next} className="right-arrow">
-              &gt;
-            </button>
-            :
-            <div></div>
+            //if it's a big screen 
+            bigScreen && (currentIndex < ((length/3)-1)) &&
+              <button onClick={next} className="right-arrow">
+                &gt;
+              </button>
+
+            ||
+
+            //if it's not a big screen 
+            !bigScreen && (currentIndex < length-1) &&
+              <button onClick={next} className="right-arrow">
+                &gt;
+              </button>
           }
           </div>
         </div>
