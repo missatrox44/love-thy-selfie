6fa56cd25e0230c00f74fd4d783a5b6437d16235
A <andcooke@gmail.com>
2023-01-13 16:53:11 -0600

    swiping is working on big and small screens currently

--- src/components/ReviewCarousel/index.jsx
+++ src/components/ReviewCarousel/index.jsx
@@ -10,9 +10,41 @@ const ReviewCarousel = (props) => {
 
   const bigScreen = useMediaQuery('(min-width:640px)');
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const minSwipeDistance = 50 
+
   const [currentIndex, setCurrentIndex] = useState(0)
   const [length, setLength] = useState(children.length)
 
+  
+  const onTouchStart = (e) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe) {
+      console.log('swipe', isLeftSwipe ? 'left' : 'right')
+      isLeftSwipe ? setCurrentIndex(prevState => prevState + 1) : setCurrentIndex(prevState => prevState - 1)
+    }
+    // console.log('swipe', isLeftSwipe ? 'left' : 'right')
+    // add your conditional logic here
+    // if (isLeftSwipe && currentIndex < (currentIndex.length - 1)) {
+    //   setCurrentIndex(prevState => prevState + 1);
+    //   console.log(currentIndex)
+    // };
+    // if (isRightSwipe && currentIndex > 0) {
+    //   setCurrentIndex(prevState => prevState - 1);
+    //   console.log(currentIndex)
+    // }
+  }
 
   // Set the length to match current children from props
   useEffect(() => {
@@ -34,9 +66,9 @@ const ReviewCarousel = (props) => {
   return (
     <div className="carousel-container">
       
-      <div className="carousel-wrapper p-5">
+      <div className="carousel-wrapper p-5" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
         {
-          currentIndex > 0 &&
+          bigScreen && currentIndex > 0 &&
             <svg onClick={prev} className="left-arrow" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 000-.708L5.207 8l2.647-2.646a.5.5 0 10-.708-.708l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M15 8a.5.5 0 00-.5-.5h-9a.5.5 0 000 1h9A.5.5 0 0015 8z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 011 13V3a1.5 1.5 0 011.5-1.5h8A1.5 1.5 0 0112 3v1.5a.5.5 0 01-1 0V3a.5.5 0 00-.5-.5h-8A.5.5 0 002 3v10a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-1.5a.5.5 0 011 0V13a1.5 1.5 0 01-1.5 1.5h-8z" clip-rule="evenodd"></path></svg>
         }
         <div className="carousel-content-wrapper">
@@ -57,8 +89,7 @@ const ReviewCarousel = (props) => {
           ||
 
           //if it's not a big screen 
-          !bigScreen && (currentIndex < length - 1) &&
-          <svg  onClick={next} className="right-arrow" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.146 11.354a.5.5 0 010-.708L10.793 8 8.146 5.354a.5.5 0 11.708-.708l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M1 8a.5.5 0 01.5-.5h9a.5.5 0 010 1h-9A.5.5 0 011 8z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M13.5 14.5A1.5 1.5 0 0015 13V3a1.5 1.5 0 00-1.5-1.5h-8A1.5 1.5 0 004 3v1.5a.5.5 0 001 0V3a.5.5 0 01.5-.5h8a.5.5 0 01.5.5v10a.5.5 0 01-.5.5h-8A.5.5 0 015 13v-1.5a.5.5 0 00-1 0V13a1.5 1.5 0 001.5 1.5h8z" clip-rule="evenodd"></path></svg>
+          !bigScreen && (currentIndex < length - 1)
         }
       </div>
     </div>
