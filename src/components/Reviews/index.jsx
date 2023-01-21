71aec40cc0de59c9d5dd9bc80ddbda5720138e8b
A <andcooke@gmail.com>
2023-01-21 10:59:23 -0600

    work on these stars

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReviewCard from '../ReviewCard';
 import ReviewCarousel from '../ReviewCarousel';
+import Star from '../../assets/Star'
 import { reviews } from "../../assets/reviews";
-import BsStarFill from '../../assets/Star';
 
 import './styles.css';
 
@@ -17,15 +17,18 @@ export default function Reviews() {
             saying!
         </div>
           <div className='flex place-content-center'>
-            <p className='font-bold mr-3'>Overall Rating:</p>
-              <BsStarFill />
-              <BsStarFill />
-              <BsStarFill />
-              <BsStarFill />
-              <BsStarFill />
+            <p className='font-bold mr-1'>Overall Rating:</p>
+            <div className="flex gap-0.5 text-yellow-500 mb-4 place-content-start h-5 w-5">
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+            </div>
+
             <p className='text-xs ml-3'>5.0/5.0</p>
             <p className="ml-3">117 Reviews</p>
-          </div>
+            </div>
       <ReviewCarousel style={{ marginLeft:'auto', marginRight:'auto', marginTop: 64 }}>
 
         {reviews.map((review, i) => {
