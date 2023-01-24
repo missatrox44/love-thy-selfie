3e0affe4434e6a531bd577f6ce7ca869a9489249
A <andcooke@gmail.com>
2023-01-24 12:14:19 -0600

    landing screen rough draft

--- src/components/LandingScreen/index.jsx
+++ src/components/LandingScreen/index.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import BookButton from '../BookButton';
 import Header from "../../components/Header";
-import LandingImage from "../../assets/LandingImage.png"
+import LargeLanding from "../../assets/LargeLanding.png"
+import PhoneLanding from '../../assets/PhoneLanding.png'
+import { useMediaQuery } from '@mui/material';
+
 
 
 export default function LandingScreen() {
+
+  const bigScreen = useMediaQuery('(min-width:1024px)');
+
   return (
     <div className="h-screen">
       <Header />
-      <div>
-        <img src={LandingImage} className="absolute"/>
-        <div className="absolute bg-gradient-to-t from-red-500 h-[25%] bottom-[30%] w-full"></div>
-      </div>
+
+      {bigScreen && <div className="flex justify-center">
+        <img src={LargeLanding} className="w-[100vw]"/>
+        <div className="absolute bg-gradient-to-t from-purple-800 h-[40%] bottom-[30%] w-full"></div>
+      </div>}
       
-      <div className="bg-red-500 h-[30%] w-full absolute bottom-0">
-        <h3 className='text-3xl font-bold mb-8 px-5 pt-5'>Do your 'selfie' a favor and book us!</h3>
-        <BookButton  className="absolute"/>
+      {!bigScreen && <div className="flex justify-center">
+        <img src={PhoneLanding} className="w-[100vw]"/>
+        <div className="absolute bg-gradient-to-t from-purple-800 h-[40%] bottom-[30%] w-full"></div>
+      </div>}
+
+      <div className="bg-purple-800 h-[30%] w-full absolute bottom-0">
+        <h3 className='text-white text-3xl font-bold mb-8 px-5 pt-5'>Do your 'selfie' a favor and book us!</h3>
+        <BookButton className="absolute"/>
       </div>
 
     </div>
