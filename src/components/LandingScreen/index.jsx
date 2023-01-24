98c15b1dbe5dbe83e33dd2e727a1326f424ee58f
A <andcooke@gmail.com>
2023-01-24 11:40:26 -0600

    landing screen

--- src/components/LandingScreen/index.jsx
+++ src/components/LandingScreen/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import BookButton from '../BookButton';
 import Header from "../../components/Header";
+import LandingImage from "../../assets/LandingImage.png"
 
 
 export default function LandingScreen() {
   return (
-    <div className="h-screen bg-gray-300">
+    <div className="h-screen">
       <Header />
-      <h3 className='text-3xl font-bold mb-8 px-5'>Do your 'selfie' a favor and book us!</h3>
-      <BookButton />
+      <div>
+        <img src={LandingImage} className="absolute"/>
+        <div className="absolute bg-gradient-to-t from-red-500 h-[25%] bottom-[30%] w-full"></div>
+      </div>
+      
+      <div className="bg-red-500 h-[30%] w-full absolute bottom-0">
+        <h3 className='text-3xl font-bold mb-8 px-5 pt-5'>Do your 'selfie' a favor and book us!</h3>
+        <BookButton  className="absolute"/>
+      </div>
+
     </div>
   )
 }
