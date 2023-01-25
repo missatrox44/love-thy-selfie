f49caca27c102907bcf8b38c4863a04123497b47
A <andcooke@gmail.com>
2023-01-25 16:26:06 -0600

    buildilng up photo gallery with links

--- src/assets/photoGallery.js
+++ src/assets/photoGallery.js
@@ -1,5 +1,86 @@
 export const photos = [
   {
-    imageId: '1nwA6-ScrCGZmf6IPwUlhUj71fFel5eGG'
+    imageId: "1nwA6-ScrCGZmf6IPwUlhUj71fFel5eGG",
+    event: "wedding"
   },
+  {
+    imageId: "1OUMtLDYEyU9xAXXjEraADbUQi5Op_3m4",
+    event: "wedding"
+  },
+  {
+    imageId: "1g7wtRtigZCy3CGysz2khksFzWurXnTsa",
+    event: "wedding"
+  },
+  {
+    imageId: "12zd86NOGzCdmHix47treLdgOeFv_tRQs",
+    event: "wedding"
+  },
+  {
+    imageId: "14Z4UoEfyLvwawiFBqHZfnCmDGIGSbnPm",
+    event: "wedding"
+  },
+  {
+    imageId: "1OMu-YcQCJEBIHNTaIRSQxhXe55RiYtFQ",
+    event: "wedding"
+  },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
+  // {
+  //   imageId:
+  //   event:
+  // },
 ]
