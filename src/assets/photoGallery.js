55a52cb8710600f597b80eed1e850e3ddefd0f5e
A <andcooke@gmail.com>
2023-01-25 18:17:25 -0600

    images added to photoGallery.js

--- src/assets/photoGallery.js
+++ src/assets/photoGallery.js
@@ -1,86 +1,222 @@
 export const photos = [
   {
     imageId: "1nwA6-ScrCGZmf6IPwUlhUj71fFel5eGG",
-    event: "wedding"
+    event: "Weddings"
   },
   {
     imageId: "1OUMtLDYEyU9xAXXjEraADbUQi5Op_3m4",
-    event: "wedding"
+    event: "Weddings"
   },
   {
     imageId: "1g7wtRtigZCy3CGysz2khksFzWurXnTsa",
-    event: "wedding"
+    event: "Weddings"
   },
   {
     imageId: "12zd86NOGzCdmHix47treLdgOeFv_tRQs",
-    event: "wedding"
+    event: "Weddings"
   },
   {
     imageId: "14Z4UoEfyLvwawiFBqHZfnCmDGIGSbnPm",
-    event: "wedding"
+    event: "Weddings"
   },
   {
     imageId: "1OMu-YcQCJEBIHNTaIRSQxhXe55RiYtFQ",
-    event: "wedding"
+    event: "Weddings"
   },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
-  // {
-  //   imageId:
-  //   event:
-  // },
+  {
+    imageId: "1ZAm21dGsX7Alw7XYIN9qd5lpctIt6Tcv",
+    event: "School Events"
+  },
+  {
+    imageId: "1_lh6qilO7coHWAJVWckoYMdyEqQTBv9p",
+    event: "School Events"
+  },
+  {
+    imageId: "1BC9UaPP17-RtD3hooNZZpbh7YgVkNFha",
+    event: "School Events"
+  },
+  {
+    imageId: "1QW235i1-81lp5CgZ8GSjxPHG0XCaBZrE",
+    event: "School Events"
+  },
+  {
+    imageId: "13RWUdxkwGF1ZX0-YYpSuGMzdMAYLDkp9",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "14NTO-pnRRAJT27mClPax5OM8E3eswlIH",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1AVes8Va9bj0YY1qGwORXTbcXCLuxgUuc",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1CJc68aii7ujV5TXSbfCHy8VXEVQNwbWk",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1D4r64cZoAv73zlaLc9t024b9ZPmbtCk4",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1OWX1HjgqeRUlx7YlBok_570co8h03DdJ",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1VJvEnG9TkKUu5haD_3mV7n69U5ePftpl",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1aH5h8Vc_9bFapAOkHqoGI71-Rw4k4OT6",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1diXm0Q79Lua3uHPP6kV_X4Vqor3Ymvn1",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1iL0BWOhuzp5B0O43GOUpveI3-pBUZDDA",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1iR8wF8VP72f3foByrLpDbticImlYnfcw",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1jxCCZVTuTKrZTQ7WyJB1APJ_1cCK3Hmc",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1lLXF_THf9PMTTM23QTYLp4_6-eZtsfIE",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1se5kpHsEAsK8qYpYMVvxfpqmKencDOIZ",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1yenfe8gq097L7tGXODCiJL8Ip0E1B6ua",
+    event: "Holiday Parties"
+  },
+  {
+    imageId: "1e3eG7TAZcvNeeCaiK8OwUrxWQUfL_z5p",
+    event: "Graduations"
+  },
+  {
+    imageId: "1HIaF5RCFIBlp6-sgqGDjwFNcRUoEGBYx",
+    event: "Graduations"
+  },
+  {
+    imageId: "1u23jNoEvRtrTuZMCxasFobfzC0rRgv-i",
+    event: "Graduations"
+  },
+  {
+    imageId: "1jrDX2dQAn7ncSSA1PpVq4iiqlk4U8s2c",
+    event: "Graduations"
+  },
+  {
+    imageId: "10Wbp92pJkUBoLdCax8adhtPH-M8Z3tXM",
+    event: "Graduations"
+  },
+  {
+    imageId: "1hfu2mh9WYA0eKdP9q0hybQfD2colDI70",
+    event: "Baby Showers"
+  },
+  {
+    imageId: "1nWF2UzQNn7uLQYkOUMqhZpmeeabAG0k4",
+    event: "Baby Showers"
+  },
+  {
+    imageId: "18FLOpU9wrFli4wFqoeuo82_ZhPH1GlNP",
+    event: "Birthdays"
+  },
+  {
+    imageId: "18lh8vzDjR74nFz64gglX7HcdlW6qEBaK",
+    event: "Birthdays"
+  },
+  {
+    imageId: "19VqMwB5VXEN3wI2eKfqRI3gzRdHY8n11",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1AE_SvlR98UyZjb3B1hS0CJu7dD3pHiPk",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1DQ7DYci8Spz4fH5yxYPLC1EgCjav_maj",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1GHR0pXxoL5-3M7KXE-QEFh0aniRntFdz",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1HpB4F6Ol9lkVupOturlhN8G3UdhPliC4",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1J4JMm5ZXwZk7d7cKfQX-A8_fgiE4seLH",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1JTJyaU2JtIslkO5H5wIfg6rAp8nHJgSA",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1KdlTsRVsNZbnFVNjpu3Jr54VLpGK1jgQ",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1LEUz1Aeq8YPZQ0iSD0NdqHkpx9NGLRM3",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1M3L6UTyVZr4X3b6LcJp4XxRof3IwlLSW",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1Q0MWxEKUiqqfoSTxtkErNCHA1zImt0gm",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1SjEvaaJ9osaOCstSREG1orUOBpKiJC3R",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1TLmXGjisEygYMyLody6W6atFlGsQ2znc",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1VRjnPJgW6YZFFWRQmbj-M_WWBS9OnPCt",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1YlMyfWGxwvSVoSAMsNb2uhLiJ5lWjyVy",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1amL_iatjBQqi7mnYAwBKKCcsOqyK_t80",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1an7f9TME8N6-nP1cq-3djIp09nJfVb3Y",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1jsD-w47J2yd7UbOf3_wMbDTYM8JGaWcg",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1nNciIDxV2YGL7iM0qYTcb2xG8QB229sK",
+    event: "Birthdays"
+  },
+    {
+    imageId: "1nqr56xy9YDT7DGJ86chaZUFJj2s2a-qz",
+    event: "Birthdays"
+  },
+  {
+    imageId: "1rC0Coj2y7G2dDWYbIMLRBAzZvc3vhcLV",
+    event: "Birthdays"
+  },
 ]
