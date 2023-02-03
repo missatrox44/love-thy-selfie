4113734bfc6a311c109929d10a551a88f58e8262
A <andcooke@gmail.com>
2023-02-03 16:12:48 -0600

    added photo gallery back

---
+++ src/components/PhotoGallery/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Photos from '../Photos';
+
+export default function PhotoGallery() {
+
+
+  return (
+    <div className = "flex justify-center py-10">
+      <Photos />
+    </div>
+  )
+}
