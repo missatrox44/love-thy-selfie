dc23a977248db1f04e6934c361b04e61780a4d12
A <andcooke@gmail.com>
2023-02-26 11:18:22 -0600

    added phone link / contact link to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -28,14 +28,14 @@ function Footer() {
             <HashLink to='/#reviews' className='text-light'>Reviews</HashLink>
             </p>
             <p className='mb-4'>
-              <Link to='/contact' className='text-light'>Contact</Link>
+              <Link to='/contact' className='text-light'>Book Now</Link>
             </p>
           </div>
 
 
           <div>
             <h6 className='uppercase font-semibold mb-4 flex justify-center md:justify-start text-2xl'>
-              Contact
+              <Link to="/contact">Contact</Link>
             </h6>
             <p className='flex items-center justify-center md:justify-start mb-4'>
               <a target='_blank' href='https://www.facebook.com/people/Love-Thy-Selfie-Photo-Booth/100063804810096/'><svg aria-hidden='true' focusable='false' data-prefix='fas' data-icon='home'
@@ -67,7 +67,7 @@ function Footer() {
                   d='M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z'>
                 </path>
               </svg>
-              (325) 514-5998
+              <a href="tel:+13255145998">(325) 514-5998</a>
             </p>
 
           </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,7 +29,7 @@ export default function Header() {
             aria-expanded='false'
             onClick={() => setIsBurgerOpen((prev) => !prev)}>
             <span className='sr-only'>Open main menu</span>
-            <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path></svg>
+            <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clipRule='evenodd'></path></svg>
           </button>
 
           <div className={isBurgerOpen ? 'showMenuNav' : 'hideMenuNav'}>
