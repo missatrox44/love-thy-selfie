7c3f6e8821e229d7b04702a2f6a172d8bcde6d55
Sara Baqla <missatrox44@gmail.com>
2023-10-23 21:15:08 -0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,24 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 
 import ScrollToTop from './components/ScrollToTop';
-
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-
   return (
-    <Router>
-      {/*  possibly add max width to app-container lg:max-w-[80%] */}
-      <ScrollToTop />
-      <div className='app-container'>
-        <Routes>
-          <Route 
-            path ="/"
-            element={<Home />}
-          />
-          <Route
-            path="/contact"
-            element={<Contact/>} 
-          />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <>
+      <Analytics />
+      <Router>
+        {/*  possibly add max width to app-container lg:max-w-[80%] */}
+        <ScrollToTop />
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
